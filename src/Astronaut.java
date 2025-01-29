46779e2c8bc9e0d910aa124da80af6e671aa0534
ecaterina99 <tricolicikatea1999@gmail.com>
2025-01-29 22:33:43 +0200

    Remove incorect methods

--- src/Astronaut.java
+++ src/Astronaut.java
@@ -1,6 +1,5 @@
 import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 
 public class Astronaut {
     private String name;
@@ -40,33 +39,9 @@ public class Astronaut {
     }
 
 
-    public void addAstronaut(List<Astronaut> astronauts,Astronaut astronaut){
-        System.out.println("Please, add astronaut name: ");
-        Scanner sc = new Scanner(System.in);
-        astronaut.setName(sc.nextLine());
-        System.out.println("Please, add astronaut age: ");
-        astronaut.setAge(sc.nextInt());
-        sc.nextLine();
-        System.out.println("Please, add astronaut nationality: ");
-        astronaut.setNationality(sc.nextLine());
-        System.out.println("Please, add astronaut years of experience: ");
-        astronaut.setYearsOfExperience(sc.nextInt());
-
-        astronauts.add(astronaut);
-        System.out.println("Astronaut added successfully!");
-    }
-
     @Override
     public String toString(){
         return "Astronaut name: " + getName()+ ", astronaut age: "+ getAge()+ ", astronaut nationality: "+ getNationality()+ ", astronaut years of experience: " + getYearsOfExperience();
-    }
-
-    public void displayAstronauts(List<Astronaut> astronauts){
-        Iterator<Astronaut> iterator = astronauts.iterator();
-        while (iterator.hasNext()) {
-            Astronaut eachAstronaut = iterator.next();
-            System.out.println(eachAstronaut);
-        }
     }
 
     public boolean isEligibleForMission(int yearsOfExperience){
