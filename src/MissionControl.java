da7b3b8fc6d51247b5accf292649153c87339f2f
ecaterina99 <tricolicikatea1999@gmail.com>
2025-01-29 22:38:57 +0200

    Implement addAstronaut and displayAstronauts methods

--- src/MissionControl.java
+++ src/MissionControl.java
@@ -1,3 +1,45 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class MissionControl {
+    List<Astronaut> astronauts = new ArrayList<>();
+
+    public void addAstronaut() {
+        Astronaut astronaut = new Astronaut();
+
+        System.out.println("Please, add astronaut name: ");
+        Scanner sc = new Scanner(System.in);
+        astronaut.setName(sc.nextLine());
+
+        System.out.println("Please, add astronaut age: ");
+        astronaut.setAge(sc.nextInt());
+        sc.nextLine();
+
+        System.out.println("Please, add astronaut nationality: ");
+        astronaut.setNationality(sc.nextLine());
+
+        System.out.println("Please, add astronaut years of experience: ");
+        astronaut.setYearsOfExperience(sc.nextInt());
+
+        this.astronauts.add(astronaut);
+        System.out.println("Astronaut added successfully!");
+    }
+
+
+    public void displayAstronauts() {
+        if (this.astronauts.isEmpty()) {
+            System.out.println("Astronaut list is empty!");
+        } else {
+            Iterator<Astronaut> iterator = astronauts.iterator();
+            while (iterator.hasNext()) {
+                Astronaut eachAstronaut = iterator.next();
+                System.out.println(eachAstronaut);
+            }
+        }
+    }
+
 
 }
