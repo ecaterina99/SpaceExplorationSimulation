6d71bb66ee6c400af64410aa7b3f690de287f4e5
ecaterina99 <tricolicikatea1999@gmail.com>
2025-01-30 16:11:29 +0200

    Add enum AllowedNationality

--- src/Astronaut.java
+++ src/Astronaut.java
@@ -1,51 +1,56 @@
-import java.util.Iterator;
-import java.util.List;
-
 public class Astronaut {
+
     private String name;
     private int age;
-    private String nationality;
+    private AllowedNationality nationality;
     private int yearsOfExperience;
+    public enum AllowedNationality {MOLDAVIAN, AMERICAN, ITALIAN, RUSSIAN}
 
     public Astronaut() {
+        super();
     }
 
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
-    public int getAge(){
+
+    public int getAge() {
         return age;
     }
 
-    public void setNationality(String nationality){
+    public void setNationality(AllowedNationality nationality) {
         this.nationality = nationality;
     }
-    public String getNationality(){
+
+    public AllowedNationality getNationality() {
         return nationality;
     }
 
-    public void setYearsOfExperience(int years){
+    public void setYearsOfExperience(int years) {
         this.yearsOfExperience = years;
     }
-    public int getYearsOfExperience(){
+
+    public int getYearsOfExperience() {
         return yearsOfExperience;
     }
 
 
     @Override
-    public String toString(){
-        return "Astronaut name: " + getName()+ ", astronaut age: "+ getAge()+ ", astronaut nationality: "+ getNationality()+ ", astronaut years of experience: " + getYearsOfExperience();
+    public String toString() {
+        return "Astronaut name: " + getName() + ", astronaut age: " + getAge() + ", astronaut nationality: " + getNationality() + ", astronaut years of experience: " + getYearsOfExperience();
     }
 
-    public boolean isEligibleForMission(int yearsOfExperience){
-        if(getYearsOfExperience() >= 5){
+    public boolean isEligibleForMission(int yearsOfExperience) {
+        if (getYearsOfExperience() >= 5) {
             return true;
         }
         return false;
